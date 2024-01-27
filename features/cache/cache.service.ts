b16e549515615c83cf76a70f104646820c5f3009
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-01-27 09:06:43 +0500

    fix: redis url added

--- features/cache/cache.service.ts
+++ features/cache/cache.service.ts
@@ -1,7 +1,9 @@
 import { RedisClientType, createClient } from "redis";
 
 // Create redis client for cashing
-const client: RedisClientType = createClient();
+const client: RedisClientType = createClient({
+  url: process.env.REDIS_URL,
+});
 client.connect();
 client.on("error", (err: any) => console.log("Redis Client Error", err));
 
